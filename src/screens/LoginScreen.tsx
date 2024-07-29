39f3a69f14fbfb5c6eebe74a732265fde9a3bf88
bheem.negi <bheem.negi@fusionmicrofinance.com>
2024-07-29 08:11:08 +0530

    feature: modify login screen

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,9 +1,18 @@
-import {Image, SafeAreaView, Text, TouchableOpacity, View} from 'react-native';
+import {
+  Image,
+  SafeAreaView,
+  StatusBar,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import FontAwesome6 from 'react-native-vector-icons/FontAwesome6';
 
 const LoginScreen = () => {
   return (
-    <SafeAreaView style={{flex: 1, backgroundColor: '#efefef'}}>
+    <SafeAreaView style={{flex: 1, backgroundColor: 'white'}}>
+      <StatusBar backgroundColor={'white'} />
+
       <View style={{width: '100%', paddingVertical: 10}}>
         <FontAwesome6
           name={'x-twitter'}
@@ -12,85 +21,131 @@ const LoginScreen = () => {
           size={22}
         />
       </View>
-      <View style={{flex: 1, backgroundColor: '#efefef'}}>
+      <View style={{flex: 1, backgroundColor: 'white'}}>
         <View
           style={{
             height: '60%',
-            backgroundColor: '#efefef',
+            backgroundColor: 'white',
             justifyContent: 'center',
           }}>
           <View style={{marginHorizontal: 30}}>
-            <Text style={{fontSize: 34, fontWeight: '600', color: 'black'}}>
+            <Text style={{fontSize: 33.5, fontWeight: '500', color: 'black'}}>
               See what's happening in the world right now
             </Text>
           </View>
         </View>
-        <View style={{height: '40%', backgroundColor: '#efefef'}}>
-          <TouchableOpacity
-            style={{
-              alignSelf: 'center',
-              borderRadius: 20,
-              paddingVertical: 10,
-              width: '80%',
-              justifyContent: 'center',
-              flexDirection: 'row',
-              backgroundColor: 'white',
-            }}>
-            <Image
-              style={{width: 24, height: 24}}
-              source={require('../assets/google.jpg')}
-            />
-            <Text
+        <View style={{height: '40%', backgroundColor: 'white'}}>
+          <View style={{flex: 1, justifyContent: 'flex-end'}}>
+            <TouchableOpacity
               style={{
                 alignSelf: 'center',
-                fontWeight: '500',
-                marginLeft: 8,
-                color: 'black',
+                borderRadius: 25,
+                paddingVertical: 7,
+                width: '80%',
+                borderWidth: 1,
+                borderColor: '#c0c0c0',
+                justifyContent: 'center',
+                flexDirection: 'row',
+                backgroundColor: 'white',
               }}>
-              Continue with Google
-            </Text>
-          </TouchableOpacity>
-          <Text style={{alignSelf: 'center', fontSize: 12}}>or</Text>
-          <TouchableOpacity
-            style={{
-              alignSelf: 'center',
-              borderRadius: 20,
-              paddingVertical: 10,
-              width: '80%',
-              justifyContent: 'center',
-              flexDirection: 'row',
-              backgroundColor: 'black',
-            }}>
-            <Text
+              <Image
+                style={{width: 24, height: 24}}
+                source={require('../assets/google.jpg')}
+              />
+              <Text
+                style={{
+                  alignSelf: 'center',
+                  fontWeight: '500',
+                  marginLeft: 8,
+                  color: 'black',
+                }}>
+                Continue with Google
+              </Text>
+            </TouchableOpacity>
+            <View
               style={{
+                flexDirection: 'row',
+                width: '80%',
                 alignSelf: 'center',
-                fontWeight: '500',
-                marginLeft: 8,
-                color: 'white',
+                paddingVertical: 8,
               }}>
-              Create Account
-            </Text>
-          </TouchableOpacity>
-          <View style={{alignSelf: 'center', width: '80%', marginTop: 30}}>
-            <Text
+              <View
+                style={{
+                  backgroundColor: '#c0c0c0',
+                  height: 1,
+                  flex: 1,
+                  alignSelf: 'center',
+                }}
+              />
+              <Text
+                style={{
+                  alignSelf: 'center',
+                  fontSize: 12,
+                  paddingHorizontal: 8,
+                  color: '#c0c0c0',
+                }}>
+                or
+              </Text>
+              <View
+                style={{
+                  backgroundColor: '#c0c0c0',
+                  height: 1,
+                  flex: 1,
+                  alignSelf: 'center',
+                }}
+              />
+            </View>
+            <TouchableOpacity
               style={{
                 alignSelf: 'center',
-                fontSize: 12,
+                borderRadius: 20,
+                paddingVertical: 10,
+                width: '80%',
+                justifyContent: 'center',
+                flexDirection: 'row',
+                backgroundColor: 'black',
+              }}>
+              <Text
+                style={{
+                  alignSelf: 'center',
+                  fontWeight: '500',
+                  marginLeft: 8,
+                  color: 'white',
+                }}>
+                Create Account
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <View
+            style={{
+              flex: 1,
+              justifyContent: 'space-between',
+              paddingBottom: 30,
+            }}>
+            <View style={{alignSelf: 'center', width: '80%', marginTop: 30}}>
+              <Text
+                style={{
+                  alignSelf: 'center',
+                  fontSize: 13,
+                  textAlign: 'left',
+                }}>
+                By signing up, you agree to our Terms,{' '}
+                <Text style={{color: '#20a7db'}}>Privacy Policy,</Text> and{' '}
+                <Text style={{color: '#20a7db'}}>Cookie Use.</Text>
+              </Text>
+            </View>
+            <Text
+              style={{
                 textAlign: 'left',
+                marginTop: 30,
+                width: '80%',
+                fontSize: 14,
+                alignSelf: 'center',
               }}>
-              By signing up, you agree to our Terms, Privacy Policy, and Cookie
-              Use.
+              Have an account already?{' '}
+              <Text style={{color: '#20a7db'}}>Log in</Text>
             </Text>
           </View>
-          <Text
-            style={{
-              textAlign: 'left',
-              marginTop: 30,
-              width: '80%',
-              alignSelf: 'center',
-            }}>
-            Have an accont already? Log in
-          </Text>
         </View>
       </View>
     </SafeAreaView>
